ca2f0d2bc27322e5279173f7284b45634cd86327
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-10-27 22:53:26 +0500

    Task2

---
+++ 231/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _231
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 4;
+            int c = 8;
+            int d = 7;
+            int i = 1;
+            do
+            {
+                b++;
+                a -= b;
+                c += a;
+                d = (c - a) + i;
+                i++;
+            } while (i < 4);
+            Console.WriteLine(d);
+        }
+    }
+}
