a324dd0cfc9af0bae3b790cd81e7971fdf9f2a87
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-10-29 22:58:04 +0500

    Task7

---
+++ Tyuiu.BushlyaYaV.Sprint3.Task7.V27.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+
+using Tyuiu.BushlyaYaV.Sprint3.Task7.V27.Lib;
+
+namespace Tyuiu.BushlyaYaV.Sprint3.Task7.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.BushlyaYaV.Sprint3.Task7.V27/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using Tyuiu.BushlyaYaV.Sprint3.Task7.V27.Lib;
+
+namespace Tyuiu.BushlyaYaV.Sprint3.Task7.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Бушля Я. В. | ИСТНб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Бушля Ярослав Владимирович | ИСТНб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции            *");
+            Console.WriteLine("* на заданном диапазоне [-5; 5] с шагом 1                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+---------+---------");
+            Console.WriteLine("|    x    |   f(x) |");
+            Console.WriteLine("+---------+---------");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}    | {1, 5:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+---------+---------");
+            Console.ReadKey();
+        }
+    }
+}
